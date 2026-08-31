e36a05261448b2e4c93c9249d0f3f06b821ff288
Mahmoud Refaey <dev.mahmoudrefaey@gmail.com>
2026-08-31 06:46:51 +0300

    fix:  embedding module

--- basic_rag/indexing/embedder.py
+++ basic_rag/indexing/embedder.py
@@ -2,17 +2,21 @@ from sentence_transformers import SentenceTransformer
 from basic_rag.indexing.chunker import chunk_pdfs
 import numpy as np
 
-model = SentenceTransformer('Omartificial-Intelligence-Space/Arabic-Triplet-Matryoshka-V2')
+def generate_embeddings(
+    data_path: str = "data",
+    model_name: str = "Omartificial-Intelligence-Space/Arabic-Triplet-Matryoshka-V2",
+    batch_size: int = 32
+):
+    model = SentenceTransformer(model_name)
+    chunks = chunk_pdfs(data_path)
+    chunks_text = [doc.page_content for doc in chunks]
 
-chunks = chunk_pdfs("data")
-chunks_text = [doc.page_content for doc in chunks]
+    embeddings = model.encode(
+        chunks_text,
+        convert_to_tensor=False,
+        batch_size=batch_size,
+        show_progress_bar=True
+    )
 
-# Generate embeddings (default of Matryoshka is 768 dims)
-embeddings = model.encode(
-    chunks_text,
-    convert_to_tensor=False,
-    batch_size=32,
-    show_progress_bar=True
-)
-
-print(f"Embedding Shape: {embeddings.shape}")
+    print(f"Embedding Shape: {embeddings.shape}")
+    return embeddings, chunks
